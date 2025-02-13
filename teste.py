12229727607d97a4bae203e3727fffb27c418941
Brutaro <brutx@Brutaros-MacBook-Pro.local>
2025-02-13 14:21:32 -0300

    https://github.com/brutaro/gpt_email_api

---
+++ teste.py
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "https://github.com/brutaro/gpt_email_api"
+git branch -M main
+git remote add origin https://github.com/brutaro/gpt_email_api.git
+git push -u origin main
